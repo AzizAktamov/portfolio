b6160608ec43f93de9916d078ab7d129edc5f17b
Azizbeksolo <bekcame77@gmail.com>
2024-12-04 17:22:05 +0500

    commit

--- mysite/bot.py
+++ mysite/bot.py
@@ -2,6 +2,6 @@ import requests
 from config.settings import BOT_TOKEN,CHAT_ID
 
 def send_message(name,email,phone_numer,description):
-    text = f"Ism:{name}\nEmail:{email}\ntel raqam {phone_numer}\ntext ; {description}"
+    text = f"NAME : {name}\nEMAIL : {email}\nPHONE : {phone_numer}\nTEXT : {description}"
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/SendMessage?chat_id={CHAT_ID}&text={text}"
     requests.get(url)
